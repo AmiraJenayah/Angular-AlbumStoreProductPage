887fc49c8a273eebe490213e26f8b70486433533
AmiraJenayah <amirajnh@outlook.fr>
2019-10-30 00:05:18 +0100

    'Completedmodule7'

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -11,7 +11,7 @@ private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) {}
 
-    getAlbum(id: number): Observable<Album>{
+    getAlbum(id:number): Observable<Album>{
     return this._http.get(this._albumUrl).map(response =>
     <Album>response.json());
   };
